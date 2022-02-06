b4d859e396405af80cb91446312ffac412089f28
muwongelawrence-tech <muwongelawrence44@gmail.com>
2022-02-06 23:41:26 +0300

    cloning google2.0

---
+++ components/Avatar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Avatar({ url }) {
+  return (<img 
+     loading='lazy'
+     src = { url }
+     className='h-10 rounded-full  cursor-pointer  transition duration-150 transform hover:scale-110'
+     alt = "profile pic"
+    />
+
+
+  );
+}
+
+export default Avatar;
